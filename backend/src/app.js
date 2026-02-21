c17093059284498abaaa6e837df385b32383d5b4
JayantPatle <patlejayant6@gmail.com>
2026-02-21 12:28:45 +0530

    minor changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors({
   origin: "https://codebase-qa-frontend.vercel.app",        // Automatically allow request origin
-  credentials: true
+  credentials: false,                                      // No cookies or credentials needed
 }));
 
 // Body parsers
